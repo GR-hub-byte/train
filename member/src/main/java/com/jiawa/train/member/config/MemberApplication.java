889f35e286924c5abebe276f0e609e88eee7ab08
Administrator <1844301051@qq.com>
2024-08-23 11:22:20 +0800

    4.4 修改启动日志 增加/member 修改Logback日志

--- member/src/main/java/com/jiawa/train/member/config/MemberApplication.java
+++ member/src/main/java/com/jiawa/train/member/config/MemberApplication.java
@@ -1,8 +1,10 @@
 package com.jiawa.train.member.config;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.core.env.Environment;
 
 /**
  * @author 小小低头哥
@@ -12,9 +14,13 @@ import org.springframework.context.annotation.ComponentScan;
  */
 @SpringBootApplication
 @ComponentScan("com.jiawa.train.member")
+@Slf4j
 public class MemberApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(MemberApplication.class);
+        SpringApplication app = new SpringApplication(MemberApplication.class);
+        Environment env = app.run(args).getEnvironment();
+        log.info("启动成功！！");
+        log.info("地址：\thttp://127.0.0.1:{}",env.getProperty("server.port"));
     }
 }
