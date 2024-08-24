11883819681f0c1496894eaa4d07d397cb0f96c5
Administrator <1844301051@qq.com>
2024-08-24 22:36:55 +0800

    4.9 增加网关模块gateway

---
+++ gateway/src/main/java/com/jiawa/train/gateway/config/GatewayApplication.java
@@ -0,0 +1,26 @@
+package com.jiawa.train.gateway.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.core.env.Environment;
+
+/**
+ * @author 小小低头哥
+ * @version v1.0
+ * @api
+ * @since 2024年 月 日
+ */
+@SpringBootApplication
+@ComponentScan("com.jiawa.train")
+@Slf4j
+public class GatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication app = new SpringApplication(GatewayApplication.class);
+        Environment env = app.run(args).getEnvironment();
+        log.info("启动成功！！");
+        log.info("网关地址：\thttp://127.0.0.1:{}",env.getProperty("server.port"));
+    }
+}
